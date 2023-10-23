9db8eb497b072f2ae4ed23b2f1bc321aa1427ab8
Kochurani <kochuranigeorge91@gmail.com>
2023-10-23 16:12:36 +0530

    change related name

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -21,6 +21,7 @@ public function addTask(Request $request)
     $user = Auth::user();
     //test change
     //test 2
+    //any change
                                     //   echo "<pre>";
                                      // print_r($user['id']);die;
                                       //print_r($user['all()']);die;   
